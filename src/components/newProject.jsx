fc1401550beec6c32f86229e8eb8f5c0cd4e4363
Kibrya <efte7123@gmail.com>
2024-10-16 06:43:28 +0600

    bug solved

--- src/components/newProject.jsx
+++ src/components/newProject.jsx
@@ -4,19 +4,31 @@ import Input from "./input.jsx";
 import Modal from "./modal.jsx";
 
 export default function NewProject({ onAdd, onCancel }) {
+  // const title = useRef();
+  // const description = useRef();
+  // const dueDate = useRef();
+  // const modal = useRef();
+
+
+
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
-  const modal = useRef();
+
   function handleSave() {
+    // bug was here (vaule----> value)
     const enteredTitle = title.current.value;
     const enteredDescription = description.current.value;
-    const enteredDueDate = dueDate.current.vaule;
+    const enteredDueDate = dueDate.current.value;
 
-    if (
-      enteredTitle.trim() === "" ||
-      enteredDescription.trim() === "" ||
-      enteredDueDate.trim() === ""
+
+
+  if (
+      enteredTitle.trim() === '' ||
+      enteredDescription.trim() === '' ||
+      enteredDueDate.trim() === ''
     ) {
       modal.current.openly();
       return;
@@ -27,8 +39,8 @@ export default function NewProject({ onAdd, onCancel }) {
       description: enteredDescription,
       dueDate: enteredDueDate,
     });
+    console.log(enteredTitle)
   }
-
   return (
     <>
       <Modal ref={modal} buttonCaption="OOkay">
@@ -60,11 +72,112 @@ export default function NewProject({ onAdd, onCancel }) {
           </li>
         </menu>
         <div>
-          <Input ref={title} label="Title" />
+          <Input type='text' ref={title} label="Title" />
           <Input ref={description} label="Description" textarea />
-          <Input ref={dueDate} label="Due Date" />
+          <Input type= 'date' ref={dueDate} label="Due Date" />
         </div>
       </div>
     </>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useRef } from 'react';
+
+// import Input from './input.jsx';
+// import Modal from './modal.jsx';
+
+// export default function NewProject({ onAdd, onCancel }) {
+//   const modal = useRef();
+
+//   const title = useRef();
+//   const description = useRef();
+//   const dueDate = useRef();
+
+//   function handleSave() {
+//     const enteredTitle = title.current.value;
+//     const enteredDescription = description.current.value;
+//     const enteredDueDate = dueDate.current.value;
+
+//     if (
+//       enteredTitle.trim() === '' ||
+//       enteredDescription.trim() === '' ||
+//       enteredDueDate.trim() === ''
+//     ) {
+//       modal.current.openly();
+//       return;
+//     }
+
+//     onAdd({
+//       title: enteredTitle,
+//       description: enteredDescription,
+//       dueDate: enteredDueDate,
+//     });
+//   }
+
+//   return (
+//     <>
+//       <Modal ref={modal} buttonCaption="Okay">
+//         <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+//         <p className="text-stone-600 mb-4">
+//           Oops ... looks like you forgot to enter a value.
+//         </p>
+//         <p className="text-stone-600 mb-4">
+//           Please make sure you provide a valid value for every input field.
+//         </p>
+//       </Modal>
+//       <div className="w-[35rem] mt-16">
+//         <menu className="flex items-center justify-end gap-4 my-4">
+//           <li>
+//             <button
+//               className="text-stone-800 hover:text-stone-950"
+//               onClick={onCancel}
+//             >
+//               Cancel
+//             </button>
+//           </li>
+//           <li>
+//             <button
+//               className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+//               onClick={handleSave}
+//             >
+//               Save
+//             </button>
+//           </li>
+//         </menu>
+//         <div>
+//           <Input type="text" ref={title} label="Title" />
+//           <Input ref={description} label="Description" textarea />
+//           <Input type="date" ref={dueDate} label="Due Date" />
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
